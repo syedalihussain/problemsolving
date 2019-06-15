b867e4f8c8c101d0c5c7635d0d80c498e26486c7
Ali <hussain.syedalishah@gmail.com>
2019-06-15 17:53:52 +1000

    Robot in a grid

---
+++ src/CtCI_Recursion_and_Dynamic_Programming/robotInAGrid_08_02.java
@@ -0,0 +1,35 @@
+package CtCI_Recursion_and_Dynamic_Programming;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class robotInAGrid_08_02 {
+
+	static class Point {
+
+		public int x;
+		public int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+
+	}
+
+	public static ArrayList<Point> getPath(boolean[][] maze) {
+		
+	}
+
+	public static void main(String[] args) {
+		HashSet<Point> failedPoints = new HashSet<>();
+
+		Point p1 = new Point(1, 2);
+		failedPoints.add(p1);
+		Point p2 = new Point(1, 2);
+		if (failedPoints.contains(p2)) {
+			System.out.println("Already present");
+		}
+	}
+}
