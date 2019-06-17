363c8b51da87c5e4ebb3bb1826ef1fbfa7a59358
Ali <hussain.syedalishah@gmail.com>
2019-06-17 19:53:42 +1000

    Towers of Hanoi Half Done

---
+++ src/CtCI_Recursion_and_Dynamic_Programming/towersOfHanoi_08_06.java
@@ -0,0 +1,51 @@
+package CtCI_Recursion_and_Dynamic_Programming;
+
+import java.util.Stack;
+
+class Tower {
+	private Stack<Integer> disks;
+
+	public Tower() {
+		disks = new Stack<>();
+	}
+
+	public void add(int d) {
+		if (!disks.isEmpty() && d >= disks.peek()) {
+			System.out.println("Error adding disk " + d);
+		} else {
+			disks.push(d);
+		}
+	}
+
+	public void moveTop(Tower destination) {
+		destination.add(disks.pop());
+	}
+
+	public void moveDisks(int numOfDisks, Tower buffer, Tower destination) {
+		for (int i = numOfDisks; i >= 0; i--) {
+
+		}
+	}
+
+}
+
+public class towersOfHanoi_08_06 {
+
+
+
+	public static void main(String[] args) {
+		int n = 3;
+		Tower[] towers = new Tower[n];
+		for (int i = 0; i < n; i++) {
+			towers[i] = new Tower();
+		}
+
+		//	Make the first tower of height h;
+		int h = 5;
+		for (int i = 5; i >= 1; i--) {
+			towers[0].add(i);
+		}
+
+		towers[0].moveDisks(h, towers[1], towers[2]);
+	}
+}
