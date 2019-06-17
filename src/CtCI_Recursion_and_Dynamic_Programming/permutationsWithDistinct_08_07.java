cc07c041e1273beb4a75597602444552305daee5
Ali <hussain.syedalishah@gmail.com>
2019-06-17 22:38:34 +1000

    Permutations of a string

---
+++ src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
@@ -0,0 +1,19 @@
+package CtCI_Recursion_and_Dynamic_Programming;
+
+import java.util.ArrayList;
+
+public class permutationsWithDistinct_08_07 {
+	ArrayList<String> getPerms(String str) {
+		char[] charArray = str.toCharArray();
+		ArrayList<String> result = new ArrayList<>();
+		getPerms(charArray, result);
+		return result;
+	}
+
+	private void getPerms(char[] charArray, ArrayList<String> result) {
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
