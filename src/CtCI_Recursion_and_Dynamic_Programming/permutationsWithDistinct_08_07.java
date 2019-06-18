81d3e924a8a2e08809ab3c16a3775b3095ea1dea
Ali <hussain.syedalishah@gmail.com>
2019-06-18 20:06:55 +1000

    Working on Permutations of a String

--- src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
+++ src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
@@ -1,6 +1,7 @@
 package CtCI_Recursion_and_Dynamic_Programming;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class permutationsWithDistinct_08_07 {
 	ArrayList<String> getPerms(String str) {
@@ -14,6 +15,17 @@ public class permutationsWithDistinct_08_07 {
 	}
 
 	public static void main(String[] args) {
+		List<List<Integer>> res = new ArrayList<>();
+		List<Integer> list1 = new ArrayList<>();
+		list1.add(1);
+		res.add(list1);
+		List<Integer> list2 = new ArrayList<>(list1);
+		list2.add(2);
+		res.add(list2);
+//		System.out.println(res);
+		System.out.println(list2);
+		list2.remove(list2.size() - 1);
+		System.out.println(list2);
 		
 	}
 }
