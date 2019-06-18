3f160d17b4c8cf328e02dce0edb8ef4c1a789a55
Ali <hussain.syedalishah@gmail.com>
2019-06-18 21:58:19 +1000

    Permutations of duplicate integers

---
+++ src/LeetCode/permutationsII.java
@@ -0,0 +1,54 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class permutationsII {
+	public static List<List<Integer>> permuteUnique(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return null;
+		HashMap<Integer, Integer> countMap = getHashMap(nums);
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
+		permuteUnique(nums.length, list, result, countMap, 0);
+		return result;
+	}
+
+	private static void permuteUnique(int len, List<Integer> list, List<List<Integer>> result, HashMap<Integer,Integer> countMap, int startIndex) {
+		if (startIndex == len) {
+			result.add(new ArrayList<>(list));
+			return;
+		}
+
+		for (Integer i : countMap.keySet()) {
+			int count = countMap.get(i);
+			if (count > 0) {
+				countMap.put(i, count - 1);
+				list.add(count);
+				permuteUnique(len, list, result, countMap, startIndex + 1);
+				list.remove(list.size() - 1);
+				countMap.put(i, count);
+			}
+		}
+	}
+
+	public static HashMap<Integer, Integer> getHashMap(int[] nums) {
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int i : nums) {
+			hm.compute(i, (key, val) -> {
+				if (val == null) {
+					return 1;
+				} else {
+					return val + 1;
+				}
+			});
+		}
+		return hm;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
