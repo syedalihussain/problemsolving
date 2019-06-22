d81b3596d8894e5937cde6769dda1e9a54e84a1f
Ali <hussain.syedalishah@gmail.com>
2019-06-22 18:23:59 +1000

    Coin Change 2 Complete

--- src/LeetCode/coinChangeII.java
+++ src/LeetCode/coinChangeII.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class coinChangeII {
 	public static int change(int amount, int[] coins) {
-		int[][] map = new int[coins.length + 1][amount];
+		int[][] map = new int[coins.length + 1][amount + 1];
 		//	Fill in the first row and first column
-		for (int i = 0; i < map.length; i++) {
-			map[0][i] = 1;
+		for (int i = 0; i <= coins.length; i++) {
+			map[i][0] = 1;
 		}
 		findCombinations(amount, coins, map);
 		return map[coins.length][amount];
@@ -17,20 +17,22 @@ public class coinChangeII {
 
 	public static void findCombinations(int amount, int[] coins, int[][] map) {
 		int coin;
-		for (int i = 1; i < map[0].length; i++) {
+		for (int i = 1; i < map.length; i++) {
 			coin = coins[i - 1];
-			for (int j = 1; j < map.length; j++) {
+			for (int j = 1; j < map[0].length; j++) {
 				if ((j - coin) < 0)
-					continue;
-				map[i][j] = map[i-1][j] + map[i][j-coin];
+					map[i][j] = map[i-1][j];
+				else
+					map[i][j] = map[i-1][j] + map[i][j-coin];
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] coins = new int[]{1,2,5};
-		System.out.println(change(5, coins));
-
+		//	5000
+		//	[11,24,37,50,63,76,89,102]
+		int[] coins = new int[]{11,24,37,50,63,76,89,102};
+		System.out.println(change(5000, coins));
 
 	}
 }
