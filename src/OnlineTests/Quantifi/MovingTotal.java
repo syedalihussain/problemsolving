a8aa772f5b8547b7e6f4d745e60acd26cea43e8a
Ali <hussain.syedalishah@gmail.com>
2019-06-22 21:38:52 +1000

    Moving total done

--- src/OnlineTests/Quantifi/MovingTotal.java
+++ src/OnlineTests/Quantifi/MovingTotal.java
@@ -1,5 +1,47 @@
 package OnlineTests.Quantifi;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MovingTotal {
 
+	private HashSet<Integer> set;
+	private Queue<Integer> queue;
+	private int movingTotal;
+
+	public MovingTotal() {
+		set = new HashSet<>();
+		queue = new LinkedList<>();
+		movingTotal = 0;
+	}
+
+	public void append(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			movingTotal += array[i];
+			queue.add(array[i]);
+			if (queue.size() == 3) {
+				set.add(movingTotal);
+				movingTotal -= queue.remove();
+			}
+		}
+	}
+
+	public boolean contains(int num) {
+		return set.contains(num);
+	}
+	public static void main(String[] args) {
+
+		MovingTotal movingTotal = new MovingTotal();
+
+		movingTotal.append(new int[]{1, 2, 3});
+
+		System.out.println(movingTotal.contains(6));
+		System.out.println(movingTotal.contains(9));
+
+		movingTotal.append(new int[]{4});
+		System.out.println(movingTotal.contains(9));
+
+	}
 }
