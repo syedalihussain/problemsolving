9b53fbdf2feaea8ee525ec265411ad33736e6741
Ali <hussain.syedalishah@gmail.com>
2019-07-03 21:55:05 +1000

    Summing a 2d array using java streams

--- src/Java8/Streams.java
+++ src/Java8/Streams.java
@@ -1,5 +1,22 @@
 package Java8;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Streams {
 
+	public static void main(String[] args) {
+		int[][] myIntArray = {
+				{ 1 },
+				{ 1 },
+				{ 1 },
+				{ 1 }
+		};
+
+		int sum = Arrays.stream(myIntArray)
+				.mapToInt(arr->IntStream.of(arr).sum()) // sum the inner array
+				.sum();
+
+		System.out.println(sum);
+	}
 }
