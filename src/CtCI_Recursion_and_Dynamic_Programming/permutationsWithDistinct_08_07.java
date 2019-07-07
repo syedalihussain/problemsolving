f9b240d86061565fc27b951193711dfe20c23314
Ali <hussain.syedalishah@gmail.com>
2019-07-07 22:57:37 +1000

    With duplicates done as well but need to implement using hashmap

--- src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
+++ src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
@@ -4,31 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class permutationsWithDistinct_08_07 {
-	ArrayList<String> getPerms(String str) {
+	public static ArrayList<String> getPerms(String str) {
 		char[] charArray = str.toCharArray();
 		ArrayList<String> permutations = new ArrayList<>();
 		getPerms(permutations, charArray, 0);
 		return permutations;
 	}
 
-	private void getPerms(ArrayList<String> permutations, char[] charArray, int startIndex) {
+	private static void getPerms(ArrayList<String> permutations, char[] charArray, int startIndex) {
 		if (startIndex == charArray.length) {
 			permutations.add(new String(charArray));
 			return;
 		}
 
 		for (int i = startIndex; i < charArray.length; i++) {
-			
+			swap(charArray, startIndex, i);
+			getPerms(permutations, charArray, startIndex+1);
+			swap(charArray, startIndex, i);
 		}
 	}
 
-	private void swap(char[] chars, int l, int r) {
+	private static void swap(char[] chars, int l, int r) {
 		char c = chars[l];
 		chars[l] = chars[r];
 		chars[r] = c;
 	}
 
 	public static void main(String[] args) {
-
+		System.out.println(getPerms("abc"));
 	}
 }
---
+++ src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDuplicates_08_08.java
@@ -0,0 +1,42 @@
+package CtCI_Recursion_and_Dynamic_Programming;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class permutationsWithDuplicates_08_08 {
+
+	public static ArrayList<String> getPerms(String s) {
+		char[] charArray = s.toCharArray();
+		HashSet<String> set = new HashSet<>();
+		ArrayList<String> permutations = new ArrayList<>();
+		getPerms(permutations, set, charArray, 0);
+		return permutations;
+	}
+
+	private static void getPerms(ArrayList<String> permutations, HashSet<String> set, char[] charArray, int startIndex) {
+		if (startIndex == charArray.length) {
+			String s = new String(charArray);
+			if (!set.contains(s)) {
+				set.add(s);
+				permutations.add(s);
+			}
+			return;
+		}
+
+		for (int i = startIndex; i < charArray.length; i++) {
+			swap(charArray, startIndex, i);
+			getPerms(permutations, set, charArray, startIndex+1);
+			swap(charArray, startIndex, i);
+		}
+	}
+
+	private static void swap(char[] chars, int l, int r) {
+		char c = chars[l];
+		chars[l] = chars[r];
+		chars[r] = c;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getPerms("aaaa"));
+	}
+}
