7cbcd034c74f5c864e17644346d6ef45d6f874e5
Ali <hussain.syedalishah@gmail.com>
2019-07-07 22:14:44 +1000

    Fastest method of finding permutations

--- src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
+++ src/CtCI_Recursion_and_Dynamic_Programming/permutationsWithDistinct_08_07.java
@@ -6,12 +6,26 @@ import java.util.List;
 public class permutationsWithDistinct_08_07 {
 	ArrayList<String> getPerms(String str) {
 		char[] charArray = str.toCharArray();
-		ArrayList<String> result = new ArrayList<>();
-		getPerms(charArray, result);
-		return result;
+		ArrayList<String> permutations = new ArrayList<>();
+		getPerms(permutations, charArray, 0);
+		return permutations;
 	}
 
-	private void getPerms(char[] charArray, ArrayList<String> result) {
+	private void getPerms(ArrayList<String> permutations, char[] charArray, int startIndex) {
+		if (startIndex == charArray.length) {
+			permutations.add(new String(charArray));
+			return;
+		}
+
+		for (int i = startIndex; i < charArray.length; i++) {
+			
+		}
+	}
+
+	private void swap(char[] chars, int l, int r) {
+		char c = chars[l];
+		chars[l] = chars[r];
+		chars[r] = c;
 	}
 
 	public static void main(String[] args) {
