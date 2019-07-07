cd91575abe42b80efdfaed872ead690b375dfc8e
Ali <hussain.syedalishah@gmail.com>
2019-07-07 22:10:22 +1000

    Permutation of string

--- src/Java8/lessionOne/Greeter.java
+++ src/Java8/lessionOne/Greeter.java
@@ -7,6 +7,10 @@ public class Greeter {
 	}
 
 	public static void main(String[] args) {
-
+		StringBuilder sb = new StringBuilder("bc");
+		StringBuilder sb2 = new StringBuilder("ab");
+		StringBuilder sb3 = sb.append(sb2);
+		sb2.deleteCharAt(sb2.length()-1);
+		System.out.println(sb2.toString());
 	}
 }
--- src/Java8/lessionOne/HelloWorldGreeting.java
+++ src/Java8/lessionOne/HelloWorldGreeting.java
@@ -5,5 +5,7 @@ public class HelloWorldGreeting implements Greeting {
 	@Override
 	public void perform() {
 		System.out.println("Hello World!");
+
+
 	}
 }
