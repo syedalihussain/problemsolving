e4b0a3c2b441933502bbae14662cb11b19ebc8e5
Ali <hussain.syedalishah@gmail.com>
2019-07-11 22:58:33 +1000

    Employee Class

---
+++ src/Java8/Employee.java
@@ -0,0 +1,14 @@
+package Java8;
+
+public class Employee {
+	private int id;
+	private String name;
+	private double salary;
+
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+
+	}
+}
+
--- src/Java8/Streams.java
+++ src/Java8/Streams.java
@@ -7,6 +7,12 @@ import java.util.stream.IntStream;
 
 public class Streams {
 
+	private static Employee[] arrayOfEmps = {
+			new Employee(1, "Jeff Bezos", 100000.0),
+			new Employee(2, "Bill Gates", 200000.0),
+			new Employee(3, "Mark Zuckerberg", 300000.0)
+	};
+
 	public static void main(String[] args) {
 		int[][] myIntArray = {
 				{ 1 },
