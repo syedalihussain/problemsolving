d18899e818f1f68e550eeb624e02ebbc346c883d
Ali <hussain.syedalishah@gmail.com>
2019-07-21 21:07:18 +1000

    Type Inference in Java Lambda

---
+++ src/Java8/lessionOne/TypeInference.java
@@ -0,0 +1,15 @@
+package Java8.lessionOne;
+
+public class TypeInference {
+
+
+	interface StringLengthLambda {
+		int getLength(String s);
+	}
+
+
+	public static void main(String[] args) {
+
+	}
+
+}
