5b1142510bbe29ec1069ff3e109cea8f1895284c
Ali <hussain.syedalishah@gmail.com>
2019-07-24 23:44:10 +1000

    java 8 flatmap

--- src/Java8/FilterAndCollect.java
+++ src/Java8/FilterAndCollect.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FilterAndCollect {
 
@@ -15,6 +16,21 @@ public class FilterAndCollect {
 													.collect(Collectors.toList());
 
 		result.forEach(System.out::println);
+
+		List<String> stringList = new ArrayList<String>();
+
+		stringList.add("One flew over the cuckoo's nest");
+		stringList.add("To kill a muckingbird");
+		stringList.add("Gone with the wind");
+
+		Stream<String> stream = stringList.stream();
+
+		stream.flatMap((value) -> {
+			String[] split = value.split(" ");
+			return (Stream<String>) Arrays.asList(split).stream();
+		})
+				.forEach((value) -> System.out.println(value))
+		;
 
 	}
 
