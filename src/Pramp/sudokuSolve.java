74cf2aabb607771507758d64099454aec7a8e13a
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2019-08-25 22:30:14 +1000

    Sudoku Solver Complete

--- src/Pramp/sudokuSolve.java
+++ src/Pramp/sudokuSolve.java
@@ -5,15 +5,10 @@ import java.util.HashSet;
 import java.util.List;
 
 public class sudokuSolve {
-
-
-	ArrayList<HashSet<Integer>> boxSets;
-	ArrayList<HashSet<Integer>> colSets;
-	ArrayList<HashSet<Integer>> rowSets;
-	public void solveSudoku(char[][] board) {
-		// HashSet<Integer>[] boxSets = new HashSet[9];
-		// HashSet<Integer>[] colSets = new HashSet[9];
-		// HashSet<Integer>[] rowSets = new HashSet[9];
+	static ArrayList<HashSet<Integer>> boxSetsG;
+	static ArrayList<HashSet<Integer>> colSetsG;
+	static ArrayList<HashSet<Integer>> rowSetsG;
+	public static void solveSudoku(char[][] board) {
 		ArrayList<HashSet<Integer>> boxSets = new ArrayList<>();
 		ArrayList<HashSet<Integer>> colSets = new ArrayList<>();
 		ArrayList<HashSet<Integer>> rowSets = new ArrayList<>();
@@ -64,41 +59,91 @@ public class sudokuSolve {
 				}
 			}
 		}
-		this.boxSets = boxSets;
-		this.colSets = colSets;
-		this.rowSets = rowSets;
-		solveSudoku(min_i, min_j, board);
-		// HashSet<Integer> set = boxSets.get(7);
-		// for (Integer i : set) {
-		//     System.out.print(i + ", ");
-		// }
-		// System.out.println("i: " + min_i + ", j: " + min_j);
+		boxSetsG = boxSets;
+		colSetsG = colSets;
+		rowSetsG = rowSets;
+		solveSudokuFwd(0, 0, board);
 	}
-	public void solveSudoku(int i, int j, char[][] board) {
-		HashSet<Integer> rowSet = rowSets.get(i);
-		HashSet<Integer> colSet = colSets.get(j);
-		HashSet<Integer> boxSet = boxSets.get(i/3*3+j/3);
-		for (int i = 1; i < 9; i++) {
-			if (!colSet.contains(i) && !rowSet.contains(i) && !boxSet.contains(i)) {
-				colSet.add(i); rowSet.add(i); boxSet.add(i);
+	public static void solveSudoku(int i, int j, char[][] board) {
+		HashSet<Integer> rowSet = rowSetsG.get(i);
+		HashSet<Integer> colSet = colSetsG.get(j);
+		HashSet<Integer> boxSet = boxSetsG.get(i/3*3+j/3);
+		for (int n = 1; n <= 9; n++) {
+			if (!colSet.contains(n) && !rowSet.contains(n) && !boxSet.contains(n)) {
+				colSet.add(n); rowSet.add(n); boxSet.add(n);
+				board[i][j] = (char) (n + '0');
 				if (solveSudokuFwd(i, j+1, board) && solveSudokuBwd(i, j-1, board)) {
 					return;
 				}
-				colSet.remove(i); rowSet.remove(i); boxSet.remove(i);
+				colSet.remove(n); rowSet.remove(n); boxSet.remove(n);
 			}
 		}
-		if ((i == 0 && j == -1) || (i == 8 && j == 9)) {
+	}
+	public static boolean solveSudokuFwd(int i, int j, char[][] board) {
+		if (i == 9) {
+			return true;
+		} else if (j == 9) {
+			return solveSudokuFwd(i+1, 0, board);
+		} else if (board[i][j] != '.') {
+			return solveSudokuFwd(i, j+1, board);
+		}
+		HashSet<Integer> rowSet = rowSetsG.get(i);
+		HashSet<Integer> colSet = colSetsG.get(j);
+		HashSet<Integer> boxSet = boxSetsG.get(i/3*3+j/3);
+		for (int n = 1; n <= 9; n++) {
+			if (!rowSet.contains(n) && !colSet.contains(n) && !boxSet.contains(n)) {
+				rowSet.add(n); colSet.add(n); boxSet.add(n);
+				board[i][j] = (char) (n + '0');
+				if (solveSudokuFwd(i, j+1, board)) {
+					return true;
+				}
+				rowSet.remove(n); colSet.remove(n); boxSet.remove(n);
+				board[i][j] = '.';
+			}
+		}
+		return false;
+	}
+	public static boolean solveSudokuBwd(int i, int j, char[][] board) {
+		if (i == -1) {
 			return true;
 		} else if (j == -1) {
-			return solveSudoku(i-1, 8, board);
-		} else if (j == 9) {
-			return
+			return solveSudokuBwd(i-1, 8, board);
+		} else if (board[i][j] != '.') {
+			return solveSudokuBwd(i, j-1, board);
 		}
+		HashSet<Integer> rowSet = rowSetsG.get(i);
+		HashSet<Integer> colSet = colSetsG.get(j);
+		HashSet<Integer> boxSet = boxSetsG.get(i/3*3+j/3);
+		for (int n = 1; n <= 9; n++) {
+			if (!rowSet.contains(n) && !colSet.contains(n) && !boxSet.contains(n)) {
+				rowSet.add(n); colSet.add(n); boxSet.add(n);
+				board[i][j] = (char) (n + '0');
+				if (solveSudokuBwd(i, j-1, board)) {
+					return true;
+				}
+				rowSet.remove(i); colSet.remove(i); boxSet.remove(i);
+				board[i][j] = '.';
+			}
+		}
+		return false;
 	}
 	public static void main(String[] args) {
-		System.out.println((char) 49);
-		System.out.println((char) 57);
-		List<Character> list = new ArrayList<>();
-		System.out.println(list.size());
+		char[][] board = new char[][]	{
+										{'5','3','.','.','7','.','.','.','.'},
+										{'6','.','.','1','9','5','.','.','.'},
+										{'.','9','8','.','.','.','.','6','.'},
+										{'8','.','.','.','6','.','.','.','3'},
+										{'4','.','.','8','.','3','.','.','1'},
+										{'7','.','.','.','2','.','.','.','6'},
+										{'.','6','.','.','.','.','2','8','.'},
+										{'.','.','.','4','1','9','.','.','5'},
+										{'.','.','.','.','8','.','.','7','9'}};
+		solveSudoku(board);
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				System.out.print(board[i][j] + ", ");
+			}
+			System.out.println();
+		}
 	}
 }
